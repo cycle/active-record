35e496b10b2218e5da22d88a018f20c0ac301d1e
roxblnfk <roxblnfk@ya.ru>
2024-05-04 16:49:14 +0400

    feat: Add ActiveRepository draft

---
+++ src/Query/ActiveRepository.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cycle\ActiveRecord\Query;
+
+use Cycle\ActiveRecord\Facade;
+use Cycle\ORM\Select;
+
+/**
+ * @template TEntity of object
+ */
+class ActiveRepository {
+    /** @var Select<TEntity> */
+    private Select $select;
+
+    /**
+     * @param class-string<TEntity> $class
+     */
+    final public function __construct(string $class)
+    {
+        $orm = Facade::getOrm();
+        $this->select = new Select($orm, $class);
+    }
+
+    /**
+     * ActiveQuery is always immutable by default.
+     */
+    public function __clone()
+    {
+        $this->select = clone $this->select;
+    }
+
+    /**
+     * Get selector associated with the repository.
+     *
+     * @return Select<TEntity>
+     */
+    public function select(): Select
+    {
+        return clone $this->select;
+    }
+
+    public function findByPK($id): ?object
+    {
+        return $this->select()->wherePK($id)->fetchOne();
+    }
+
+    public function findOne(array $scope = []): ?object
+    {
+        return $this->select()->fetchOne($scope);
+    }
+
+    public function findAll(array $scope = [], array $orderBy = []): iterable
+    {
+        return $this->select()->where($scope)->orderBy($orderBy)->fetchAll();
+    }
+}
