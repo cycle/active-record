3ccfb5ab39e342d067489a051abf9fb455413a37
roxblnfk <roxblnfk@ya.ru>
2024-05-31 00:32:03 +0400

    chore: move AR exception interface into Exception namespace

--- src/Exception/ConfigurationException.php
+++ src/Exception/ConfigurationException.php
@@ -4,6 +4,4 @@ declare(strict_types=1);
 
 namespace Cycle\ActiveRecord\Exception;
 
-use Cycle\ActiveRecord\Contract\ActiveRecordException;
-
 class ConfigurationException extends \RuntimeException implements ActiveRecordException {}
